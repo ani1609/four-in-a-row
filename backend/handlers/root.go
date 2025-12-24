a5e37b55c93f6712aae470ce18f5e4b56644235c
Ankit Kr. Chowdhury <rakesh856100@gmail.com>
2025-12-24 16:59:59 +0530

    fix: update greeting message in RootHandler for testing purposes

--- backend/handlers/root.go
+++ backend/handlers/root.go
@@ -13,7 +13,7 @@ type RootResponse struct {
 // RootHandler returns a simple greeting
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	response := RootResponse{
-		Message: "Hello from Four in a Row Server!",
+		Message: "Hello from Four in a Row Server! (Test)",
 	}
 
 	w.Header().Set("Content-Type", "application/json")
